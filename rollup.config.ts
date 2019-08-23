46b4f47ec8ce66494087816ddca954452907339e
Juan Marín <juanpablo.marin76@gmail.com>
2019-08-23 05:33:05 -0500

    build(rollup): remove unused plugins for building

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
+const dependencies = Object.keys(pkg.dependencies || {});
 
 const libraryName = 'reactive-commons'
 
@@ -15,30 +12,15 @@ export default {
     { file: pkg.main, name: camelCase(libraryName), format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'esm', sourcemap: true }
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [
-    'amqplib',
-    'amqp-connection-manager',
-    'uuid',
-    'url',
-    'events'
+    ...dependencies,
+    'events',
+    'url'
   ],
   watch: {
     include: 'src/**'
   },
   plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve({ preferBuiltins: true }),
-
-    // Resolve source maps to the original source
-    sourceMaps()
+    typescript({ useTsconfigDeclarationDir: true })
   ]
 }
